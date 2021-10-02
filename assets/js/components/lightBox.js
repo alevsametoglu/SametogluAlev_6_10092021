26c80effaa3e3d02f6d472043c47afde6c1aa9c6
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-10-02 09:29:14 +0200

    creat lightbox

---
+++ assets/js/components/lightBox.js
@@ -0,0 +1,79 @@
+export default class LightBoxComponent {
+  static getEl(source, title, type, onClickNext, onClickPrevious) {
+    const lightBoxEl = document.createElement("div");
+    lightBoxEl.classList.add("lightbox");
+    lightBoxEl.setAttribute("role", "dialog");
+
+    const closeButton = document.createElement("i");
+    closeButton.className = "fas fa-times close-lightbox-icon";
+    closeButton.setAttribute("role", "button");
+    closeButton.setAttribute("title", '"Close dialog');
+    closeButton.addEventListener("click", this.hide);
+    lightBoxEl.appendChild(closeButton);
+
+    const photoContainer = document.createElement("div");
+    photoContainer.className = "lightbox-photo";
+
+    const previousButton = document.createElement("span");
+    previousButton.addEventListener("click", onClickPrevious);
+    previousButton.innerHTML = `<i
+            class="fas fa-chevron-left left-arrow-lightbox"
+            role="button"
+            title="Previous image"
+        ></i>`;
+    photoContainer.appendChild(previousButton);
+
+    const mediaEl = document.createElement("span");
+    mediaEl.className = "media";
+
+    const figureContent =
+      type === "image"
+        ? `<img src="./assets/images/${source}" alt="${title}" />`
+        : `<video src="./assets/images/${source}" alt="${title}" controls="controls" role="button" />`;
+
+    mediaEl.innerHTML = `
+            <figure>
+               ${figureContent}
+            </figure>
+            <figcaption>${title}</figcaption>
+          `;
+    photoContainer.appendChild(mediaEl);
+
+    const nextButton = document.createElement("span");
+    nextButton.addEventListener("click", onClickNext);
+    nextButton.innerHTML = `<i
+            class="fas fa-chevron-right right-arrow-lightbox"
+            role="button"
+            title="Next image"
+        ></i>`;
+    photoContainer.appendChild(nextButton);
+
+    lightBoxEl.appendChild(photoContainer);
+    return lightBoxEl;
+  }
+
+  static updateCurrentMedia(source, title, type) {
+    console.log({ type });
+    const mediaEl = document.querySelector("div.lightbox-photo > span.media");
+    const figureContent =
+      type === "image"
+        ? `<img src="./assets/images/${source}" alt="${title}" />`
+        : `<video src="./assets/images/${source}" alt="${title}" controls="controls" role="button" />`;
+
+    mediaEl.innerHTML = `
+    <figure>
+      ${figureContent}
+    </figure>
+    <figcaption>${title}</figcaption>`;
+  }
+
+  static show() {
+    const el = document.querySelector("div.lightbox");
+    el.style.display = "flex";
+  }
+
+  static hide() {
+    const el = document.querySelector("div.lightbox");
+    el.style.display = "none";
+  }
+}
