ab005c1d3da1e60cdf7a81104bf91af4a4932caf
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-10-02 09:20:46 +0200

    toggle taglist

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,37 @@
 "use strict";
 import API from "./api.js";
 import Components from "./components.js";
+import Scroll from "./components/scroll.js";
 
 const photographerListEl = document.getElementById("photographer-list");
+const selectedTags = [];
+
+const toggleSelectedTags = (tagValue) => {
+  const index = selectedTags.indexOf(tagValue);
+  if (index === -1) selectedTags.push(tagValue);
+  else selectedTags.splice(index, 1);
+  loadTagList();
+  loadPhotographerList();
+};
+
+const loadTagList = () => {
+  const tagListEl = document.querySelector("nav .tag-list");
+  API.getTagList().then((tags) => {
+    const tagElementsData = tags.map((tag) => {
+      return {
+        value: tag,
+        isSelected: selectedTags.includes(tag),
+        toggleSelectedTags,
+      };
+    });
+
+    tagListEl.replaceWith(Components.createTagListEl(tagElementsData));
+  });
+};
 
 const loadPhotographerList = () => {
-  API.getPhotographerList().then((photographers) => {
+  API.getPhotographerListByTags(selectedTags).then((photographers) => {
+    photographerListEl.innerHTML = "";
     photographers.forEach((photographer) => {
       const photographerEl = Components.createPhotographerEl(photographer);
       photographerListEl.appendChild(photographerEl);
@@ -13,4 +39,8 @@ const loadPhotographerList = () => {
   });
 };
 
+const scrollPage = () => Scroll.scrollBtn();
+
 loadPhotographerList();
+loadTagList();
+scrollPage();
