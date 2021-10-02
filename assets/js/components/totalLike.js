516bc4c7f766d03a3b2141d34c738d6ece7bae55
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-10-02 09:27:33 +0200

    creat total like

---
+++ assets/js/components/totalLike.js
@@ -0,0 +1,19 @@
+export default class TotalLikeComponent {
+  static getEl(price, totalLike) {
+    const el = document.createElement("div");
+    el.classList.add("like-price-wrapper");
+    el.setAttribute("aria-label", "price and likes of photographers");
+    el.innerHTML = `
+          <div class="like-price">
+            <span><span class='like-count'>${totalLike}</span> <i class="fas fa-heart"></i> </span>
+            <span>${price}&euro;/jour</span>
+          </div>
+        `;
+    return el;
+  }
+
+  static updateTotalLike(totalLike) {
+    const el = document.querySelector("div.like-price>span>span.like-count");
+    el.textContent = totalLike;
+  }
+}
