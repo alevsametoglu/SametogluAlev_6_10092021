a6d362c75cb1b0551bab5bc654a7d6a15159baee
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-10-07 09:35:43 +0200

    add role

--- assets/js/components/totalLike.js
+++ assets/js/components/totalLike.js
@@ -2,7 +2,8 @@ export default class TotalLikeComponent {
   static getEl(price, totalLike) {
     const el = document.createElement("div");
     el.classList.add("like-price-wrapper");
-    el.setAttribute("aria-label", "price and likes of photographers");
+    el.setAttribute("aria-label", "price and likes");
+    el.setAttribute("role", "textbox");
     el.innerHTML = `
           <div class="like-price">
             <span><span class='like-count'>${totalLike}</span> <i class="fas fa-heart"></i> </span>
